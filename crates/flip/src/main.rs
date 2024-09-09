7804c08a1677581941e39228d09e84c973e76dac
daniel sonbolian <dsal3389@gmail.com>
2024-09-10 00:19:07 +0300

    fk this, change of direction, this is going to be a window manager

--- crates/flip/src/main.rs
+++ crates/flip/src/main.rs
@@ -1,9 +1,5 @@
 use anyhow::Context;
-use flip_libx11::x11::{Display, Screen};
 
 fn main() -> anyhow::Result<()> {
-    let display = Display::new::<&str>(None).context("couldn't get display")?;
-    let screen = Screen::from(&display);
-    println!("display {display:?} screen {screen:?}");
     Ok(())
 }
